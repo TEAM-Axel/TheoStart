acb5514cb1646b9ef7a12bacb07a6c9a5cdee45f
TEAM-Axel <152228959+TEAM-Axel@users.noreply.github.com>
2023-11-29 14:59:08 -0800

    extended Greeting block

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -12,12 +12,14 @@ const gree2 = `${CONFIG.greetingMorning}\xa0`;
 const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
+const question = `${CONFIG.greetingQuestion}\xa0`;
+
 if (hour >= 23 || hour < 6) {
-	document.getElementById('greetings').innerText = gree1 + name;
+	document.getElementById('greetings').innerText = gree1 + name + question;
 } else if (hour >= 6 && hour < 12) {
-	document.getElementById('greetings').innerText = gree2 + name;
+	document.getElementById('greetings').innerText = gree2 + name + question;
 } else if (hour >= 12 && hour < 17) {
-	document.getElementById('greetings').innerText = gree3 + name;
+	document.getElementById('greetings').innerText = gree3 + name + question;
 } else {
-	document.getElementById('greetings').innerText = gree4 + name;
+	document.getElementById('greetings').innerText = gree4 + name + question;
 }
